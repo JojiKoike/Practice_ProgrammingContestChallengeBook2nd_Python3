0f9f2cecbf0d9eb9f9d082910ba0d0026ccf0733
JojiKoike <joji.koike@gmail.com>
2018-08-19 15:41:22 +0900

    クジ引きバイナリーサーチ版

---
+++ PrepareStage/kujibiki_kai1.py
@@ -0,0 +1,30 @@
+n = int(input())
+m = int(input())
+k = [int(input()) for i in range(n)]
+k_sorted = sorted(k)
+
+
+# Search
+def binary_search(x):
+    left, right = 0, n
+    while right - left >= 1:
+        i = int((left + right) / 2)
+        if k_sorted[i] == x:
+            return True
+        elif k[i] < x:
+            left = i + 1
+        else:
+            right = i
+    return False
+
+
+# Judge
+judge_result = False
+for a in range(n):
+    for b in range(n):
+        for c in range(n):
+            if binary_search(m - k[a] - k[b] - k[c]):
+                judge_result = True
+
+# Print Result
+print("Yes" if judge_result else "No")
