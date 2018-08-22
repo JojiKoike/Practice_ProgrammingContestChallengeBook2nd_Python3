4f5d9a1a264dff675944e23279968cbe9d4666f1
JojiKoike <joji.koike@gmail.com>
2018-08-22 23:20:07 +0900

    最小ターン数計算実装完了

--- BeginnerStage/maze.py
+++ BeginnerStage/maze.py
@@ -19,12 +19,36 @@ INF = 100000000
 # Variables
 sx, sy = 0, 1
 gx, gy = m - 1, n - 2
-
+d = [[ INF for j in range(n)] for i in range(m)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 # Generate Maze Data
 maze = [['#' if random.randint(0, 1) == 1 else '.' for j in range(n)] for i in range(m)]
 maze[sx][sy] = "S"
 maze[gx][gy] = "G"
-
-
 print(maze)
+
+
+# Breadth-First Search
+def bfs() -> int:
+    queue = deque()
+    queue.append(Pair(sx, sy))
+    d[sx][sy] = 0
+
+    # Loop until queue become empty
+    while len(queue) > 0:
+        pair = queue.popleft()
+        if pair.x == gx and pair.y == gy:
+            break
+        for i in range(4):
+            nx, ny = pair.x + dx[i], pair.y + dy[i]
+            if 0 <= nx < m and 0 <= ny < m and maze[nx][ny] != '#' and d[nx][ny] == INF:
+                queue.append(Pair(nx, ny))
+                d[nx][ny] = d[pair.x][pair.y] + 1
+
+    return d[gx][gy]
+
+
+print("{0}".format(bfs()))
+
