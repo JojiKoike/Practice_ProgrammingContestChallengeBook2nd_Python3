79db6757e02848d3be26e77b69afa672cb855b90
JojiKoike <joji.koike@gmail.com>
2018-11-25 18:45:08 +0900

    KnapSack With DP Table

--- BeginnerStage/DynamicProgramming/knapsack_problem_kai2.py
+++ BeginnerStage/DynamicProgramming/knapsack_problem_kai2.py
@@ -1,13 +1,28 @@
+# Get Number of items and Limit Weight
 n, W = map(int, input().split())
-weight_value = [input().split() for i in range(n)]
-dp = [[-1 for k in range(10000 + 1)] for j in range(100 + 1)]
 
+# Get Items info and build value and weight list
+items = [map(int, input().split()) for i in range(n)]
+values = []
+weights = []
+for item in items:
+    v, w = item
+    values.append(v)
+    weights.append(w)
+
+print(values)
+print(weights)
+
+# Initialize Dynamic Table
+dp = [[0 for j in range(W + 1)] for i in range(n + 1)]
+
+# Solve
 for i in range(n - 1, -1, -1):
-    for j in range(0, W + 1):
-        if j < int(weight_value[i][0]):
+    for j in range(W + 1):
+        if j < weights[i]:
             dp[i][j] = dp[i + 1][j]
         else:
-            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j - int(weight_value[i][0])] + int(weight_value[i][1]))
-        print(dp[i][j])
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j - weights[i]] + values[i])
 
-print(dp[0][W])
+# Show Result
+print("Result : {0}".format(dp[0][W]))
